7ee742d2fe52b0124a89f5998e43ae5e156a75c7
Fabiano Aranha <45189116+gomesf126@users.noreply.github.com>
2026-06-17 18:19:52 -0300

    Trabalhando com os features

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pandas as pd
 from src.extract.extrair import extrair
-from src.pipeline import pipeline
+from src.pipeline.pipeline import pipeline , criar_resumos
 from src.analytics.metricas import criar_metricas
+
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
+
+def main():
+    df = extrair()
+    resumos = criar_resumos(df)
+    df = pipeline(df)
+
+    metrica = criar_metricas(df)
+    print(resumos)
+
+    return df
+if __name__ == '__main__':
+    main()
--- src/transform/features.py
+++ src/transform/features.py
@@ -1,35 +1,56 @@
+from src.transform.limpeza import resumo_nulos,resumo_duplicados
+
 from src.transform.limpeza import (
-padronizar_colunas,
-tratar_texto,
-tratar_numeros,
-tratar_nulos_numeros,
-tratar_negativos,
-resumo_nulos,
-flags_nulos,
-tratar_nulos,
-resumo_duplicados,
-remover_duplicatas,
-tratar_datas,
-remover_colunas_invalidas
+    padronizar_colunas,
+    tratar_texto,
+    tratar_numeros,
+    tratar_nulos_numeros,
+    tratar_negativos,
+    flags_nulos,
+    tratar_nulos,
+    remover_duplicatas,
+    tratar_datas,
+    remover_colunas_invalidas
 )
+from src.transform.feature_taxa_liquidacao import feature_taxa_liquidacao
+from src.transform.feature_taxa_pagamento import feature_taxa_pagamento
+from src.transform.feature_saldo_liquidar import feature_saldo_liquidar
+from src.transform.feature_tempo import feature_tempo
+from src.transform.feature_eficiencia_orcamentaria import feature_eficiencia_orcamentaria
+from src.transform.feature_classe_abc_valor_liquidado import feature_classe_abc_valor_liquidado
+from src.transform.feature_alertas_empenhos_altos import feature_alertas_empenhos_altos
+from src.transform.churn_inatividade_orgao import churn_inatividade_orgao
+
 CLEANING = [
     padronizar_colunas,
     tratar_texto,
     tratar_numeros,
     tratar_nulos_numeros,
     tratar_negativos,
-    resumo_nulos,
     flags_nulos,
     tratar_nulos,
-    resumo_duplicados,
     remover_duplicatas,
     tratar_datas,
     remover_colunas_invalidas
 ]
 
 ENGINEERING = [
+    feature_tempo,
+    churn_inatividade_orgao,
+    feature_classe_abc_valor_liquidado,
+    feature_taxa_liquidacao,
+    feature_taxa_pagamento,
+    feature_saldo_liquidar,
+    feature_eficiencia_orcamentaria,
+    feature_alertas_empenhos_altos
+    ]
 
-]
+
+def criar_resumos(df):
+    return {
+        'nulos': resumo_nulos(df),
+        'duplicados': resumo_duplicados(df)
+    }
 
 def criar_pipeline(df, etapas):
     for funcao in etapas:
